14cf2fc47a45b3d6aea578050c228e8cc6195f79
Nicholas Nassar <niccholaspage@gmail.com>
2012-04-06 21:21:00 -0400

    Integer support

--- src/com/niccholaspage/SandStorm/language/VariableParser.java
+++ src/com/niccholaspage/SandStorm/language/VariableParser.java
@@ -6,26 +6,30 @@ import com.niccholaspage.SandStorm.language.variables.Variable;
 public class VariableParser {
 	private final String name;
 	
-	private final Object value;
+	private Object value;
 	
 	public VariableParser(String line){
 		int index = line.lastIndexOf(Constants.EQUAL_SIGN);
 
 		name = line.substring(0, index);
 
-		String value = line.substring(index + 1);
+		String stringValue = line.substring(index + 1);
 		
 		if (SandStorm.isDebugging()){
 			System.out.println("Variable Name: " + name);
 			
-			System.out.println("Variable Value: " + value);
+			System.out.println("Variable Value: " + stringValue);
 		}
 		
-		if (value.equals("") || value.equalsIgnoreCase("null")){
-			value = null;
+		if (stringValue.equals("") || stringValue.equalsIgnoreCase("null")){
+			stringValue = null;
 		}
 		
-		this.value = value;
+		try {
+			value = Integer.parseInt(stringValue);
+		}catch (NumberFormatException e){
+			value = stringValue;
+		}
 	}
 	
 	public Variable getVariable(){
